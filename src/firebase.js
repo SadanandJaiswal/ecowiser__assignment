3621f25323b532dd6e5e4e130dca123658f55686
Sadanand Jaiswal <122560812+SadanandJaiswal@users.noreply.github.com>
2024-06-18 23:10:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,6 @@ import { getFirestore } from "firebase/firestore";
 console.log('env value is here ', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID)
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBDV8LPPMghOUdLqYUWdHQR7DrFPANUnb4",
-  // authDomain: "learning-firebase-3004.firebaseapp.com",
-  // projectId: "learning-firebase-3004",
-  // storageBucket: "learning-firebase-3004.appspot.com",
-  // messagingSenderId: "701830552098",
-  // appId: "1:701830552098:web:13975373ed304d0b65087b",
-  // databaseURL: "https://learning-firebase-3004-default-rtdb.firebaseio.com",
-  // measurementId: "G-ZHG8BRPFC1",
-
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
